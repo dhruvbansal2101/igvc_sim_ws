de37d1eb5749a67e91687d70bfeb0402ca175855
dhruvbansal2101 <bansaldhruv1404@gmail.com>
2026-02-13 04:11:53 +0530

    changing sensors functionality to make them work

--- install/igvc_bringup/share/igvc_bringup/launch/gazebo.launch.py
+++ install/igvc_bringup/share/igvc_bringup/launch/gazebo.launch.py
@@ -27,27 +27,35 @@ def generate_launch_description():
         ),
 
         Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher'
+        ),
+
+        Node(
             package='ros_gz_sim',
             executable='create',
             arguments=[
                 '-topic', 'robot_description',
                 '-name', 'igvc_bot',
+                '-allow_renaming', 'false',
                 '-z', '0.0'
             ],
             output='screen'
         ),
 
-        # ✅ Correct Bridge (NO TF HERE)
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            arguments=[
-                '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-                '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-                '/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
-                '/imu@sensor_msgs/msg/Imu@gz.msgs.IMU'
-            ],
-            output='screen'
-        )
+        # Correct Bridge
+ Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    arguments=[
+        '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+        '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+        '/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+        '/imu@sensor_msgs/msg/Imu@gz.msgs.IMU',
+        '/model/igvc_bot/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V'
+    ],
+    output='screen'
+)
+
 
     ])
